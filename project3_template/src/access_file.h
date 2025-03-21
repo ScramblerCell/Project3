c45721327b54461240acf304588b63ae2705bc7a
Jonah Downing <jmd205@hotmail.com>
2025-03-20 21:12:30 -0500

    implemented read_file, find_mean, and helper methods in main. Formatting is correct up to "Summarized Statistics"

--- project3_template/src/access_file.h
+++ project3_template/src/access_file.h
@@ -8,35 +8,37 @@
 
 using namespace std;
 
-/*
-function name: check_file
-arguements:
-    FILEPATH: a string of file path
-return value: a bool that indicates if file in FILEPATH is accessable or not
-*/
+/**
+ * @brief checks if FILEPATH is accessible
+ * 
+ * @param FILEPATH  string of file path
+ * @return true     FILEPATH was accessible
+ * @return false    FILEPATH wasn't accessible
+ */
 bool check_file(string FILEPATH);
 
-/*
-function name: read_file
-arguements:
-    FILEPATH: a string of file path
-    v: a reference to a vector of double
-purpose: read file in FILEPATH then parse the file and save its content into vector v
+/**
+ * @brief read file in FILEPATH then parse the file and save its content into vector v
 return value: a bool that indicates if the contents in FILEPATH is legal or not
-*/
+ * 
+ * @param FILEPATH  a string of file path
+ * @param v         a reference to a vector of double
+ * @return true     if FILEPATH is legal
+ * @return false    if FILEPATH isn't legal
+ */
 bool read_file(string FILEPATH, vector<double>& v);
 
-/*
-function name: write_file
-arguements:
-    FILEPATH: a string of file path
-    v: a sorted vector of double
-    mean: a double that is the calculated mean of v
-    median: a double that is the calculated median of v
-    mode: a double that is the calculated mode of v
-purpose: write v, mean, median and mode to file in FILEPATH
-return value: a bool that indicates the write process is successful or not
-*/
+/**
+ * @brief write v, mean, median and mode to file in FILEPATH
+ * 
+ * @param FILEPATH  a string of file path
+ * @param v         a sorted vector of double
+ * @param mean      a double that is the calculated mean of v
+ * @param median    a double that is the calculated median of v
+ * @param mode      a double that is the calculated mode of v
+ * @return true     writing process was sucessful
+ * @return false    writing process wasn't successful
+ */
 bool write_file(string FILEPATH, vector<double> v, double mean, double median, double mode);
 
 
